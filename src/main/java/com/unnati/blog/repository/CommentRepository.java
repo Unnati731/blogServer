a9780f95f9b5324b7507eafe3d61acff2012f2fa
unnati <unnatiupendra@gmail.com>
2024-08-20 15:13:37 +0530

    Finished Project

---
+++ src/main/java/com/unnati/blog/repository/CommentRepository.java
@@ -0,0 +1,13 @@
+package com.unnati.blog.repository;
+
+import com.unnati.blog.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPostId(Long postId);
+}
---
+++ src/main/java/com/unnati/blog/service/PostService.java
@@ -0,0 +1,22 @@
+package com.unnati.blog.service;
+
+import com.unnati.blog.model.Post;
+import com.unnati.blog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostService {
+    Post savePost(Post post);
+
+    List<Post> getAllPosts();
+
+    Post getPostById(Long postId);
+
+    public void likePost(Long postId);
+
+    public List<Post> searchByName(String name);
+
+}
